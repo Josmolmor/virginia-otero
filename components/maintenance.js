36cde90af780137c46701563c3242b2d621e6bd1
Molina <molina@z1.digital>
2021-11-22 23:52:40 +0100

    style: responsive tweaks

--- components/maintenance.js
+++ components/maintenance.js
@@ -16,7 +16,7 @@ export default function Maintenance() {
                 <Logo style={{ zIndex: 1, marginTop: '10rem', pointerEvents: 'none', height: "300px", width: "300px", color: "#B83E4B" }} />
                 <span style={{ margin: '2rem auto', borderTop: '1px solid rgba(0,0,0,0.05)', borderBottom: '1px solid rgba(0,0,0,0.05)', padding: '1.5rem 0', zIndex: 1, width: '80%', textAlign: 'center' }}>Página web en desarrollo</span>
                 <img style={{ position: 'fixed', bottom: 0, pointerEvents: 'none'}} src="/images/footer.png"  />
-                <span style={{ display: 'flex', zIndex: 1 }}><Instagram style={{ marginRight: '0.5rem' }} color="#B83E4B" />Mientras tanto, puedes contactar conmigo&nbsp;<a style={{ color: '#B83E4B', fontWeight: 'bold' }} href="https://www.instagram.com/laconsultadevirginia/" target="_blank">a través de instagram.</a></span>
+                <span style={{ display: 'flex', zIndex: 1, flexWrap: 'wrap', justifyContent: 'center' }}><Instagram style={{ marginRight: '0.5rem' }} color="#B83E4B" />Mientras tanto, puedes contactar conmigo&nbsp;<a style={{ color: '#B83E4B', fontWeight: 'bold' }} href="https://www.instagram.com/laconsultadevirginia/" target="_blank">a través de instagram.</a></span>
             </div>
         </Layout>
     )
