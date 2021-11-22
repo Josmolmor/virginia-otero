4d47e3ca5aec83320edb99617a4890cec7b5b335
Molina <molina@z1.digital>
2021-11-22 23:47:53 +0100

    feat: use svg and add react feather dep

--- components/maintenance.js
+++ components/maintenance.js
@@ -1,5 +1,6 @@
 import Layout from "../components/layout";
-import Container from "../components/container";
+import { Instagram} from "react-feather";
+import Logo from "./Icons/Logo";
 
 export default function Maintenance() {
     const style = {
@@ -12,9 +13,10 @@ export default function Maintenance() {
     return (
         <Layout>
             <div style={style}>
-                <img style={{ zIndex: 1, marginTop: '10rem' }} height="300px" width="300px" src="/images/maintenance.jpeg" />
-                <span style={{ marginTop: '1rem', borderTop: '1px solid rgba(0,0,0,0.05)', paddingTop: '1.5rem', zIndex: 1 }}>Página web en proceso de desarrollo</span>
-                <img style={{ position: 'fixed', bottom: 0}} src="/images/footer.png"  />
+                <Logo style={{ zIndex: 1, marginTop: '10rem', pointerEvents: 'none', height: "300px", width: "300px", color: "#B83E4B" }} />
+                <span style={{ margin: '2rem auto', borderTop: '1px solid rgba(0,0,0,0.05)', borderBottom: '1px solid rgba(0,0,0,0.05)', padding: '1.5rem 0', zIndex: 1, width: '80%', textAlign: 'center' }}>Página web en desarrollo</span>
+                <img style={{ position: 'fixed', bottom: 0, pointerEvents: 'none'}} src="/images/footer.png"  />
+                <span style={{ display: 'flex', zIndex: 1 }}><Instagram style={{ marginRight: '0.5rem' }} color="#B83E4B" />Mientras tanto, puedes contactar conmigo&nbsp;<a style={{ color: '#B83E4B', fontWeight: 'bold' }} href="https://www.instagram.com/laconsultadevirginia/" target="_blank">a través de instagram.</a></span>
             </div>
         </Layout>
     )
