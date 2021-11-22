def2e22c0f61ce947621fe0f738c1f73f06e05a8
Molina <molina@z1.digital>
2021-11-22 23:23:33 +0100

    feat: tweak seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import '../styles/index.css'
 import Maintenance from "../components/maintenance";
+import config from "../config/seo.json";
+import {DefaultSeo} from "next-seo";
 
 function MyApp({ Component, pageProps }) {
   if (process.env.NEXT_PUBLIC_MAINTENANCE === '1') {
-    return <Maintenance />;
+    return <>
+      <DefaultSeo {...config}/>
+      <Maintenance />
+    </>;
   }
-  return <Component {...pageProps} />
+  return <>
+    <DefaultSeo {...config}/>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
